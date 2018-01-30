c119c874796e97a5bcb835ee8cb22fc3ec9fc2e2
jaxsin <33134346+jaxsin@users.noreply.github.com>
2018-01-30 16:43:04 -0500

    8-2

---
+++ chapter8/8-2.py
@@ -0,0 +1,9 @@
+prefixes = "JKLMNOPQ"
+suffix = "ack"
+for letter in prefixes:
+    if letter == "O":
+        print(letter + "u" + suffix)
+    if letter == "Q":
+        print(letter + "u" + suffix)
+print(letter + suffix)
+
